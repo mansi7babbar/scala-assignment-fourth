4a73cbe3ba4bc453fe1c5d5029ab47effbc34453
mansi7babbar <mansi7babbar@gmail.com>
2020-02-09 10:32:24 +0530

    added solution of question 2

--- src/main/scala/com/knoldus/ValueClass.scala
+++ src/main/scala/com/knoldus/ValueClass.scala
@@ -1,23 +1,25 @@
-class FirstName(val fname:String) extends AnyVal {
-  def displayFirstName:String = fname
+package com.knoldus
+
+class FirstName(val fname: String) extends AnyVal {
+  def displayFirstName: String = fname
 }
 
-class LastName(val lname:String) extends AnyVal {
-  def displayLastName:String = lname
+class LastName(val lname: String) extends AnyVal {
+  def displayLastName: String = lname
 }
 
-class Age(val age:Int) extends AnyVal {
-  def displayAge:Int = age
+class Age(val age: Int) extends AnyVal {
+  def displayAge: Int = age
 }
 
-object Display extends App{
+object ValueClass extends App {
 
   val fname = scala.io.StdIn.readLine("Enter first name of the person: ")
   val lname = scala.io.StdIn.readLine("Enter last name of the person: ")
   print("Enter age of the person: ")
-  val age = scala.io.StdIn.readInt()
+  val age = scala.io.StdIn.readInt().abs
 
-  def displayDetails(firstNameObject:FirstName, lastNameObject:LastName, ageObject:Age):String = {
+  def displayDetails(firstNameObject: FirstName, lastNameObject: LastName, ageObject: Age): String = {
     val fname = firstNameObject.displayFirstName
     val lname = lastNameObject.displayLastName
     val age = ageObject.displayAge
